68a41b862e98f59f668f2099513228ac80321a83
Mads Emil <mads81i3@stud.kea.dk>
2022-08-31 16:46:38 +0200

    filter

--- babushka.js
+++ babushka.js
@@ -8,6 +8,20 @@ const options = {
 
 const main = document.querySelector("#holder");
 const template = document.querySelector("template").content;
+const header = document.querySelector("header h1");
+
+let filter = "alle";
+const filterKnapper = document.querySelectorAll("nav button");
+filterKnapper.forEach((knap) =>
+  knap.addEventListener("click", filtrerOversigt)
+);
+
+function filtrerOversigt() {
+  filter = this.dataset.kategori;
+  hentBabushkaData();
+
+  header.textContent = this.textContent;
+}
 
 async function hentBabushkaData() {
   const resspons = await fetch(url, options);
@@ -18,13 +32,16 @@ async function hentBabushkaData() {
 
 function vis(json) {
   console.log(json);
+  main.textContent = "";
   json.forEach((json) => {
-    const klon = template.cloneNode(true);
-    klon.querySelector("img").src = "billeder/" + json.billednavn + "-md.jpg";
-    klon.querySelector(".navn").textContent = json.navn;
-    klon.querySelector(".beskrivelse").textContent = json.kortbeskrivelse;
-    klon.querySelector(".pris").textContent = json.pris;
-    main.appendChild(klon);
+    if (filter == json.kategori || filter == "alle") {
+      const klon = template.cloneNode(true);
+      klon.querySelector("img").src = "billeder/" + json.billednavn + "-md.jpg";
+      klon.querySelector(".navn").textContent = json.navn;
+      klon.querySelector(".beskrivelse").textContent = json.kortbeskrivelse;
+      klon.querySelector(".pris").textContent = json.pris;
+      main.appendChild(klon);
+    }
   });
 }
 
